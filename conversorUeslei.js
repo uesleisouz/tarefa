9aa222808de7b68819d04372667031ba42de1b4f
Ueslei Carvalho <ueslei_carvalho-f@estudante.sesisenai.org.br>
2024-02-07 16:54:22 -0300

    Criado as funções do javascript

--- conversorUeslei.js
+++ conversorUeslei.js
@@ -1,2 +1,13 @@
 let resp = document.getElementById('resp')
 console.log(resp);
+
+
+function calcTempC(){
+    let tempC = Number(document.getElementById('tempC').value)
+    console.log(tempC);
+
+    let tempCalc = (tempC * 1.8) + 32
+    console.log(tempCalc);
+
+    resp.innerHTML = "O valor convertido é : °F " + tempCalc
+}
